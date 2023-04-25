ff6a4f89af16e7227f36d75c384731ddf5686aba
sanketdange <sdange2@illinois.edu>
2023-04-24 19:11:49 -0500

    initial commit

---
+++ src/secret.js
@@ -0,0 +1,12 @@
+export const firebase_config = {
+    apiKey: "AIzaSyBtEgPZpE_2CoFZVSnkN-xJ5ymggt7A3Xw",
+    authDomain: "tuneheads-music.firebaseapp.com",
+    databaseURL: "https://tuneheads-music-default-rtdb.firebaseio.com",
+    projectId: "tuneheads-music",
+    storageBucket: "tuneheads-music.appspot.com",
+    messagingSenderId: "430712081375",
+    appId: "1:430712081375:web:92d8fa42977565f0ab526a",
+    measurementId: "G-M03HRZ8NHJ"
+};
+
+export const rapid_api_key = "edfd1bdb11msh67b82ed79542273p1677b2jsn21c9c9796258";
---
+++ src/song.js
@@ -0,0 +1,9 @@
+class Song {
+    constructor(song_name, artist_names, album_url) {
+        this.song_name = song_name;
+        this.artist_names = artist_names;
+        this.album_url = album_url;
+    }
+}
+
+export default Song;
